65dc2f281fe6cbdfe6204d6030ddbca77e99f532
Matheus Pedrosa <matcres.pedrosa@gmail.com>
2026-06-10 21:34:41 -0300

    feat(example): expand example playerController script

--- src/scripts/playerController.c3dsl.ts
+++ src/scripts/playerController.c3dsl.ts
@@ -15,8 +15,8 @@ const viewportRegions = object("viewportRegions", SpriteObject);
 const ARROW_LAYER = "2";
 const ARROW_SPEED = 400;
 const ARROW_ANGLE = "angle(player.X, player.Y, Mouse.X, Mouse.Y)";
-const AIM_DELTA_X = "abs(Mouse.X - player.X)";
-const AIM_DELTA_Y = "abs(Mouse.Y - player.Y)";
+const SHOOT_ANIMATION = 'player.direction & "Arrow"';
+const SWORD_ANIMATION = 'player.direction & "Sword"';
 const NOT_SHOOTING = {
 	...player.check(SpriteObject.isBooleanInstanceVariableSet("isShooting")),
 	isInverted: true,
@@ -33,20 +33,33 @@ function disabled(action) {
 	};
 }
 
-function shootArrowActions(direction) {
+function shootArrowActions() {
 	return [
 		player.execute(SpriteObject.setBooleanInstanceVariable("isShooting", true)),
 		player.execute(SpriteObject.setAnimationSpeed("20")),
-		player.execute(SpriteObject.setAnimation(`"${direction}Arrow"`, "beginning")),
+		player.execute(SpriteObject.setAnimation(SHOOT_ANIMATION, "beginning")),
 		SystemPlugin.createObject(arrow, ARROW_LAYER, "player.X", "player.Y"),
 		arrow.execute(BulletBehavior.setSpeed(ARROW_SPEED)),
 		arrow.execute(BulletBehavior.setAngleOfMotion(ARROW_ANGLE)),
 	];
 }
 
-function finishShootActions(direction) {
+function finishShootActions() {
 	return [
-		player.execute(SpriteObject.setInstanceVariable("direction", `"${direction}"`)),
+		player.execute(SpriteObject.setBooleanInstanceVariable("isShooting", false)),
+	];
+}
+
+function swordActions() {
+	return [
+		player.execute(SpriteObject.setBooleanInstanceVariable("isShooting", true)),
+		player.execute(SpriteObject.setAnimationSpeed("20")),
+		player.execute(SpriteObject.setAnimation(SWORD_ANIMATION, "beginning")),
+	];
+}
+
+function finishSwordActions() {
+	return [
 		player.execute(SpriteObject.setBooleanInstanceVariable("isShooting", false)),
 	];
 }
@@ -76,6 +89,7 @@ sheet("playerController", [
 			block({
 				conditions: [
 					MousePlugin.cursorIsOverObject(viewportRegions),
+					NOT_SHOOTING,
 				],
 				actions: [
 					player.execute(SpriteObject.setInstanceVariable("direction", "viewportRegions.direcao")),
@@ -165,67 +179,44 @@ sheet("playerController", [
 
 	group("actions", {
 		children: [
+			group("sword", {
+				children: [
+					block("slash sword", {
+						conditions: [
+							MousePlugin.onClick("left"),
+							NOT_SHOOTING,
+						],
+						actions: swordActions(),
+					}),
+					block("finish sword", {
+						conditions: [
+							player.check(SpriteObject.onAnimationFinished(SWORD_ANIMATION)),
+						],
+						actions: finishSwordActions(),
+					}),
+				],
+			}),
 			group("shoot", {
 				children: [
-					block("shoot right arrow", {
+					block("shoot arrow", {
 						conditions: [
 							KeyboardPlugin.onKeyPressed("Q"),
 							NOT_SHOOTING,
-							SystemPlugin.compareTwoValues(AIM_DELTA_X, "greater-or-equal", AIM_DELTA_Y),
-							SystemPlugin.compareTwoValues("Mouse.X", "greater-or-equal", "player.X"),
 						],
-						actions: shootArrowActions("right"),
+						actions: shootArrowActions(),
 					}),
-					block("shoot left arrow", {
+					block("shoot arrow mouse", {
 						conditions: [
-							KeyboardPlugin.onKeyPressed("Q"),
+							MousePlugin.onClick("right"),
 							NOT_SHOOTING,
-							SystemPlugin.compareTwoValues(AIM_DELTA_X, "greater-or-equal", AIM_DELTA_Y),
-							SystemPlugin.compareTwoValues("Mouse.X", "less", "player.X"),
 						],
-						actions: shootArrowActions("left"),
+						actions: shootArrowActions(),
 					}),
-					block("shoot up arrow", {
+					block("finish arrow", {
 						conditions: [
-							KeyboardPlugin.onKeyPressed("Q"),
-							NOT_SHOOTING,
-							SystemPlugin.compareTwoValues(AIM_DELTA_Y, "greater", AIM_DELTA_X),
-							SystemPlugin.compareTwoValues("Mouse.Y", "less", "player.Y"),
+							player.check(SpriteObject.onAnimationFinished(SHOOT_ANIMATION)),
 						],
-						actions: shootArrowActions("up"),
-					}),
-					block("shoot down arrow", {
-						conditions: [
-							KeyboardPlugin.onKeyPressed("Q"),
-							NOT_SHOOTING,
-							SystemPlugin.compareTwoValues(AIM_DELTA_Y, "greater", AIM_DELTA_X),
-							SystemPlugin.compareTwoValues("Mouse.Y", "greater-or-equal", "player.Y"),
-						],
-						actions: shootArrowActions("down"),
-					}),
-					block("finish right arrow", {
-						conditions: [
-							player.check(SpriteObject.onAnimationFinished("\"rightArrow\"")),
-						],
-						actions: finishShootActions("right"),
-					}),
-					block("finish left arrow", {
-						conditions: [
-							player.check(SpriteObject.onAnimationFinished("\"leftArrow\"")),
-						],
-						actions: finishShootActions("left"),
-					}),
-					block("finish up arrow", {
-						conditions: [
-							player.check(SpriteObject.onAnimationFinished("\"upArrow\"")),
-						],
-						actions: finishShootActions("up"),
-					}),
-					block("finish down arrow", {
-						conditions: [
-							player.check(SpriteObject.onAnimationFinished("\"downArrow\"")),
-						],
-						actions: finishShootActions("down"),
+						actions: finishShootActions(),
 					}),
 				],
 			}),
